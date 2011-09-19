da441fdf170dfe0a3608fee5ee55f93b854f389c
zumbojo <zumbojo@gmail.com>
2011-09-18 22:36:03 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FeedTheDogHeroku::Application.config.secret_token = '9fde093280e629d2250d37b96fdb058161d3413a11eb1777100d4c8c46d11fc41f9c622380a319842a66320f15ac790e9be19da31d249d502ec50f2a38afdf07'
