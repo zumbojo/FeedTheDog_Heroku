1f0768cfc6c8f661716f0e5d6d55347b381f572b
zumbojo <zumbojo@gmail.com>
2011-09-18 23:12:56 -0500

    More shoehorning of jquery_testapp into this app

--- app/controllers/feedings_controller.rb
+++ app/controllers/feedings_controller.rb
@@ -1,2 +1,33 @@
 class FeedingsController < ApplicationController
+  def index
+    @feedings = Feeding.all
+    @feeding = Feeding.new
+
+    respond_to do |format|
+      format.html
+    end
+  end
+
+  def create
+    @feeding = Feeding.new(params[:post])
+
+    respond_to do |format|
+      if @feeding.save
+        format.html { redirect_to(feedings_url, :notice => 'Feeding was successfully created.') }
+        format.js
+      else
+        format.html { redirect_to(feedings_url) }
+      end
+    end
+  end
+
+  def destroy
+    @feeding = Post.find(params[:id])
+    @feeding.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(feedings_url) }
+      format.js
+    end
+  end
 end
