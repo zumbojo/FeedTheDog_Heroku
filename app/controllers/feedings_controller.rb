fc99bdae9d002b8275885d8b9cbe51c3dc16078f
zumbojo <zumbojo@gmail.com>
2011-09-21 19:55:33 -0500

    activate_feeder is working perfectly.

--- app/controllers/feedings_controller.rb
+++ app/controllers/feedings_controller.rb
@@ -36,14 +36,17 @@ class FeedingsController < ApplicationController
       @feeder_private_key = ENV['FEEDER_PRIVATE_KEY']
 
       @feeder_create_nonces_url = @feeder_url + '/nonces/create'
+      @feeder_use_nonces_url = @feeder_url + '/nonces/use'
 
       # get nonce from feeder
-      @unsigned_nonce = Net::HTTP.get_response(URI.parse(@feeder_create_nonces_url))
-      @unsigned_nonce.body
-
-      # @signed_nonce = Digest::SHA2.hexdigest(@unsigned_nonce + @feeder_private_key)
+      @unsigned_nonce = Net::HTTP.get_response(URI.parse(@feeder_create_nonces_url)).body
 
       # sign nonce
+      @signed_nonce = Digest::SHA2.hexdigest(@unsigned_nonce + @feeder_private_key)
+
       # post nonce back to feeder
+      @post_response = Net::HTTP.post_form(URI.parse(@feeder_use_nonces_url), {"signed_nonce" => @signed_nonce}).body
+
+      return @post_response == 'success'
     end
 end
